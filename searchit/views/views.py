951e2090d9ad0b9a24e4cc74eebc05bcb0b92795
Ajey Khanapuri <ajeybk1@gmail.com>
2014-10-16 15:01:13 +0530

    Added es host to development.ini file.

--- searchit/views/views.py
+++ searchit/views/views.py
@@ -1,5 +1,6 @@
 from pyramid.response import Response
 from pyramid.view import view_config
+from searchit.modules import elasticsearch_util
 
 @view_config(route_name='hello')
 def hello_world(request):
@@ -14,4 +15,7 @@ def hello_world_json(request):
 
 @view_config(route_name='search')
 def search_keys(request):
+    # print elasticsearch_util.get_es_url()
+    # print elasticsearch_util.get_es_url_for_id(0)
+    # print elasticsearch_util.get_es_host()
     return Response('Search keys')
