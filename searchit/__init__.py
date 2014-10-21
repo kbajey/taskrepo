7f4e500a219d9099cbc779f9008e9b9e8a0055a6
darshan <darshan@jutera.com>
2014-10-21 11:36:43 +0530

    adding APIs and querying support, changes for review comments

--- searchit/__init__.py
+++ searchit/__init__.py
@@ -2,7 +2,6 @@ from pyramid.config import Configurator
 from pyramid.view import notfound_view_config, forbidden_view_config
 from pyramid.response import Response
 
-
 @notfound_view_config()
 def notfound(request):
     return Response('Not Found, Try something else', status='404 Not Found')
@@ -18,5 +17,9 @@ def main(global_config, **settings):
     config.add_route('hello', '/')
     config.add_route('hello.json', 'hello.json')
     config.add_route('search', 'search')
+    config.add_route('top_keywords','top_keywords')
+    config.add_route('top_customers','top_customers')
+    config.add_route('top_users','top_users')
+    config.add_route('top_keywords_by_user','top_keywords_by_user')
     config.scan()
     return config.make_wsgi_app()
--- searchit/views/views.py
+++ searchit/views/views.py
@@ -1,6 +1,6 @@
 from pyramid.response import Response
 from pyramid.view import view_config
-from searchit.modules import elasticsearch_util
+from searchit.modules import es_aggregation_count_util
 
 @view_config(route_name='hello')
 def hello_world(request):
@@ -13,9 +13,43 @@ def hello_world_json(request):
         print "Data present"
     return {'a':1,'b':2, 'c':3, 'd':[1,2,3,4]}
 
-@view_config(route_name='search')
-def search_keys(request):
-    # print elasticsearch_util.get_es_url()
-    # print elasticsearch_util.get_es_url_for_id(0)
-    # print elasticsearch_util.get_es_host()
-    return Response('Search keys')
+@view_config(route_name='top_keywords', renderer='json')
+def get_top_searched_keywords(request):
+	'''
+	Returns top searched keywords in a given date range
+	'''
+	from_date = request.GET.get('from_date')
+	to_date = request.GET.get('to_date')
+	result = es_aggregation_count_util.get_aggcount_for_date_range(from_date, to_date, range_field='user_visit_time_date', aggs_field='keyword',aggs_size=10)
+	return result
+
+@view_config(route_name='top_customers', renderer='json')
+def get_top_customers_by_search_count(request):
+	'''
+	Returns top customers based on number of searches for them,
+	in a given date range
+	'''
+	from_date = request.GET.get('from_date')
+	to_date = request.GET.get('to_date')
+	result = es_aggregation_count_util.get_aggcount_for_date_range(from_date, to_date, range_field='user_visit_time_date', aggs_field='customer_id',aggs_size=10)
+	return result
+
+@view_config(route_name='top_users', renderer='json')
+def get_top_users_by_search_count(request):
+	'''
+	Returns top users based on number of searches performed by them,
+	in a given date range
+	'''
+	from_date = request.GET.get('from_date')
+	to_date = request.GET.get('to_date')
+	result = es_aggregation_count_util.get_aggcount_for_date_range(from_date, to_date, range_field='user_visit_time_date', aggs_field='user_id',aggs_size=10)
+	return result
+
+@view_config(route_name='top_keywords_by_user', renderer='json')
+def get_top_keywords_searched_by_user(request):
+	'''
+	Returns top keywords searched by a particular user
+	'''
+	user_id = request.GET.get('user_id')
+	result = es_aggregation_count_util.get_aggcount_for_field('user_id', user_id, aggs_field='keyword', aggs_size=10)
+	return result
